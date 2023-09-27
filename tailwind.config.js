2b0294726c449cdd3860f26d55425260c44997d8
“yatkun” <sweetninenty99@gmail.com>
2023-09-28 03:47:52 +0800

    tes

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#4f46e5',
+      },
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
