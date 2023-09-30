fdb7d2f8c56a016d87ef4f2a9bc5f7971a508626
“yatkun” <sweetninenty99@gmail.com>
2023-09-30 21:45:11 +0800

    update 2.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#4f46e5',
+        'primary': '#4f46e5', 
+        'heading': '#062f4e',
+        'text' : '#183547'
       },
       fontFamily: {
         'nunito': ['Nunito', 'sans-serif']
